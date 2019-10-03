2d3cc7990446bb20ced6d2da7b29f3529c72ba44
Frederique Hsu <frederique.hsu@outlook.com>
2019-10-04 00:26:07 +0800

    Study how to iterate on the composite data set.

--- iterate_on_composite_data.py
+++ iterate_on_composite_data.py
@@ -44,3 +44,40 @@ for i in range(len(x)):
 print("{0}\n{1}\n{2}".format(list(range(5)), 
                              list(range(9, 21)), 
                              tuple(range(10, -25, -5))))
+
+
+print(list(range(10)))
+print(list(reversed(range(10))))
+
+x = []
+for t in zip(range(-10, 0, 1), range(0, 10, 2), range(1, 10, 2)):
+    x += t
+
+print(x)
+print(sorted(x))
+print(sorted(x, reverse = True))
+print(sorted(x, key = abs))
+
+vessels = ["Sloop", "Yawl", "Cutter", "Schooner", "Ketch"]
+print(vessels)
+print(sorted(vessels, key = str.lower))
+
+temp_list = []
+for item in vessels:
+    temp_list.append((item.lower(), item))
+vessel_list = []
+for key, value in sorted(temp_list):
+    vessel_list.append(value)
+
+print(vessel_list)
+
+boats = list(zip((1, 3, 1, 3), ("pram", "dory", "kayak", "canoe")))
+print(boats)
+print(sorted(boats))
+
+def swap(t):
+    return t[1], t[0]
+
+print(sorted(boats, key = swap))
+print(sorted([3.8, -7.5, 0, 1.3, -6.4, 8.5]))
+# print(sorted([3, "spanner", -7.5, 0, 1.3]))     # TypeError: '<' not supported between instances of 'str' and 'int'
