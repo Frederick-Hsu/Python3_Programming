8f5dbdd824a493f79ecb52f097ee8d09b7681052
Frederique Hsu <frederique.hsu@outlook.com>
2019-10-04 21:12:51 +0800

    By using the try ... except ... finally structure to implement a correct file-opening-close procedure.

---
+++ Control_Flow/noblanks.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#
+# File name     : noblanks.py
+#
+
+def read_data(filename):
+    lines = []
+    fh = None
+    try:
+        fh = open(filename, encoding = "utf8")
+        for line in fh:
+            if line.strip():
+                lines.append(line)
+    except (IOError, OSError) as err:
+        print(err)
+        return []
+    except EnvironmentError as err:
+        print(err)
+        return []
+    finally:
+        if fh is not None:
+            fh.close()
+    return lines
